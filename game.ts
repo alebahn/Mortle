0b1fdb6d7cc76d9a1801d001f0ed1a6d449d9d44
Aaron Lebahn <cplusplus328@gmail.com>
2020-02-04 22:21:21 -0600

    Scale canvas and add pixelated font.

--- game.ts
+++ game.ts
@@ -2,18 +2,17 @@ class Game
 {
   private _canvasContext : CanvasRenderingContext2D;
 
-  constructor(canvasContext : CanvasRenderingContext2D)
+  constructor(canvasContext : CanvasRenderingContext2D, width: number, height: number)
   {
     this._canvasContext = canvasContext;
+    console.log(width);
+    console.log(height);
   }
 
   public ShowMenu() : void
   {
-    this._canvasContext.fillStyle = 'rgb(200, 0, 0)';
-    this._canvasContext.fillRect(10, 10, 50, 50);
-
-    this._canvasContext.fillStyle = 'rgba(0, 0, 200, 0.5)';
-    this._canvasContext.fillRect(30, 30, 50, 50);
+    this._canvasContext.font = "8px 'Press Start 2P'";
+    this._canvasContext.fillText("Hello World!", 10, 10);
   }
 }
 
@@ -28,7 +27,13 @@ window.onload = () =>
   let context = canvas.getContext("2d");
   if (context)
   {
-    let game = new Game(context);
-    game.ShowMenu();
+    let contextNotNull = context;
+    WebFont.load({
+      google: {families: ['Press Start 2P']},
+      active: function () {
+        let game = new Game(contextNotNull, canvas.width, canvas.height);
+        game.ShowMenu();
+      }
+    })
   }
 }
