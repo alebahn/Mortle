1fa92ef7e953a4df8ca7248a1ae94d3496f88b35
Aaron Lebahn <cplusplus328@gmail.com>
2020-02-05 22:13:35 -0600

    Implement game loop and beginning of state machine

--- game.ts
+++ game.ts
@@ -1,18 +1,64 @@
+enum GameState
+{
+  Startup,
+  Menu
+}
+
 class Game
 {
   private _canvasContext : CanvasRenderingContext2D;
+  private _gameState = GameState.Startup;
+  private _lastTimeStep = 0;
+  private _width : number;
+  private _height : number;
 
   constructor(canvasContext : CanvasRenderingContext2D, width: number, height: number)
   {
     this._canvasContext = canvasContext;
-    console.log(width);
-    console.log(height);
+    this._width = width;
+    this._height = height;
   }
 
-  public ShowMenu() : void
+  public DrawMenu() : void
   {
     this._canvasContext.font = "8px 'Press Start 2P'";
-    this._canvasContext.fillText("Hello World!", 10, 10);
+    this._canvasContext.fillText("Hello World!", 1, 8);
+    this._canvasContext.fillText("Hello World!", 1, 16);
+    this._canvasContext.fillText("Hello World!", 1, 24);
+    this._canvasContext.fillText("Hello World!", 1, 32);
+    this._canvasContext.fillText("Hello World!", 1, 40);
+    this._canvasContext.fillText("Hello World!", 1, 48);
+    this._canvasContext.fillText("Hello World!", 1, 56);
+    this._canvasContext.fillText("Hello World!", 1, 64);
+  }
+
+  public Start() : void
+  {
+    requestAnimationFrame(this.Loop.bind(this));
+  }
+
+  private Loop(timestamp: number) : void
+  {
+    this.Update();
+    this.Draw();
+    this._lastTimeStep = timestamp;
+    requestAnimationFrame(this.Loop.bind(this));
+  }
+
+  private Update() : void
+  {
+  }
+
+  private Draw() : void
+  {
+    switch(this._gameState)
+    {
+      case GameState.Startup:
+        this._canvasContext.clearRect(0,0,this._width, this._height);
+        this.DrawMenu();
+        this._canvasContext.save();
+        this._gameState = GameState.Menu;
+    }
   }
 }
 
@@ -23,16 +69,16 @@ class Player
 window.onload = () =>
 {
 
-  let canvas = document.getElementById("gameCanvas") as HTMLCanvasElement;
-  let context = canvas.getContext("2d");
+  const canvas = document.getElementById("gameCanvas") as HTMLCanvasElement;
+  const context = canvas.getContext("2d");
   if (context)
   {
-    let contextNotNull = context;
+    const contextNotNull = context;
     WebFont.load({
       google: {families: ['Press Start 2P']},
       active: function () {
         let game = new Game(contextNotNull, canvas.width, canvas.height);
-        game.ShowMenu();
+        game.Start();
       }
     })
   }
