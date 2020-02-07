093deda59267ba4f445d0c83851559e90d4783a2
Aaron Lebahn <cplusplus328@gmail.com>
2020-02-06 20:57:27 -0600

    Implement update and drawing. Handle up/down keypress on menu.

--- game.ts
+++ game.ts
@@ -1,81 +1,129 @@
-enum GameState
-{
-  Startup,
-  Menu
+type GameState = "Startup" | "Menu";
+
+const MenuOptions = ["New Game", "Continue"] as const;
+type MenuOption = typeof MenuOptions[number];
+
+class UnreachableCaseError extends Error {
+  constructor(val: never) {
+    super(`Unreachable case: ${val}`);
+  }
 }
 
-class Game
-{
-  private _canvasContext : CanvasRenderingContext2D;
-  private _gameState = GameState.Startup;
-  private _lastTimeStep = 0;
-  private _width : number;
-  private _height : number;
+class Game {
+  private canvasContext: CanvasRenderingContext2D;
+  private gameState : GameState = "Startup";
+  private nextState : GameState = "Startup";
+  private lastTimeStep = 0;
+  private width: number;
+  private height: number;
+  private background: ImageData;
 
-  constructor(canvasContext : CanvasRenderingContext2D, width: number, height: number)
-  {
-    this._canvasContext = canvasContext;
-    this._width = width;
-    this._height = height;
+  private menuState : MenuOption = "New Game";
+  private oldMenuState : MenuOption = "New Game";
+
+  constructor(canvasContext: CanvasRenderingContext2D, width: number, height: number) {
+    this.canvasContext = canvasContext;
+    this.width = width;
+    this.height = height;
+    this.background = new ImageData(width, height);
   }
 
-  public DrawMenu() : void
-  {
-    this._canvasContext.font = "8px 'Press Start 2P'";
-    this._canvasContext.fillText("Hello World!", 1, 8);
-    this._canvasContext.fillText("Hello World!", 1, 16);
-    this._canvasContext.fillText("Hello World!", 1, 24);
-    this._canvasContext.fillText("Hello World!", 1, 32);
-    this._canvasContext.fillText("Hello World!", 1, 40);
-    this._canvasContext.fillText("Hello World!", 1, 48);
-    this._canvasContext.fillText("Hello World!", 1, 56);
-    this._canvasContext.fillText("Hello World!", 1, 64);
+  public DrawMenu(): void {
+    this.canvasContext.font = "8px 'Press Start 2P'";
+    this.canvasContext.fillText("MORTLE", 24, 12);
+
+    this.canvasContext.fillText("New Game", 4, 24);
+    this.canvasContext.fillText("Continue", 4, 32);
   }
 
-  public Start() : void
-  {
+  private HandleKeyDown(ev: KeyboardEvent): void {
+    switch (this.gameState) {
+      case "Menu":
+        switch (ev.keyCode)
+        {
+          case 38: // Up
+            this.menuState = "New Game";
+            break;
+          case 40: // Down
+            this.menuState = "Continue";
+            break;
+        }
+        break;
+    }
+  }
+
+  private HandleKeyUp(ev: KeyboardEvent): void {
+  }
+
+  public Start(): void {
+    document.addEventListener("keydown", this.HandleKeyDown.bind(this));
+    document.addEventListener("keyup", this.HandleKeyUp.bind(this));
     requestAnimationFrame(this.Loop.bind(this));
   }
 
-  private Loop(timestamp: number) : void
-  {
+  private Loop(timestamp: number): void {
     this.Update();
     this.Draw();
-    this._lastTimeStep = timestamp;
+    this.lastTimeStep = timestamp;
     requestAnimationFrame(this.Loop.bind(this));
   }
 
-  private Update() : void
-  {
+  private Update(): void {
+    switch (this.gameState) {
+      case "Startup":
+        this.nextState = "Menu";
+        break;
+    }
   }
 
-  private Draw() : void
-  {
-    switch(this._gameState)
-    {
-      case GameState.Startup:
-        this._canvasContext.clearRect(0,0,this._width, this._height);
-        this.DrawMenu();
-        this._canvasContext.save();
-        this._gameState = GameState.Menu;
+  private Draw(): void {
+    if (this.gameState !== this.nextState) {
+      switch (this.nextState) {
+        case "Menu":
+          this.canvasContext.clearRect(0, 0, this.width, this.height);
+          this.DrawMenu();
+          this.background = this.canvasContext.getImageData(0, 0, this.width, this.height);
+          break;
+      }
+      this.gameState = this.nextState;
+    }
+
+    switch (this.gameState) {
+      case "Menu":
+        const xCoordinate = 72;
+        if (this.oldMenuState !== this.menuState) {
+          const yCoordinateOld = this.GetYCoordinateFromMenuState(this.oldMenuState);
+          this.canvasContext.fillStyle = "white";
+          this.canvasContext.fillText("<", xCoordinate, yCoordinateOld);
+          this.canvasContext.fillStyle = "black";
+          this.oldMenuState = this.menuState;
+        }
+        const yCoordinate = this.GetYCoordinateFromMenuState(this.menuState);
+        this.canvasContext.fillText("<", xCoordinate, yCoordinate);
+        break;
+    }
+  }
+  private GetYCoordinateFromMenuState(menuState: MenuOption): number {
+    switch (menuState) {
+      case "New Game":
+        return 24;
+      case "Continue":
+        return 32;
     }
   }
 }
 
-class Player
-{
+class Player {
 }
 
-window.onload = () =>
-{
+window.onload = () => {
 
   const canvas = document.getElementById("gameCanvas") as HTMLCanvasElement;
   const context = canvas.getContext("2d");
-  if (context)
-  {
+  if (context) {
     const contextNotNull = context;
     WebFont.load({
-      google: {families: ['Press Start 2P']},
+      google: { families: ['Press Start 2P'] },
       active: function () {
         let game = new Game(contextNotNull, canvas.width, canvas.height);
         game.Start();
